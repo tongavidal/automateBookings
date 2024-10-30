11df4209a65af183943489a2b7f3661db9235cb0
tongavidal <63450412+tongavidal@users.noreply.github.com>
2024-10-30 09:54:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,84 @@
-import logging
-import logging.handlers
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+import time
 import os
 
-import requests
+email = os.environ["USERNAME"]
+pwd = os.environ["PASSWORD"]
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+# Set up Chrome options
+options = webdriver.ChromeOptions()
+options.headless = False  # Change to True to run in headless mode
+
+# Initialize the Chrome driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+daysRange = 4 # Days reservation restriction
+
+def denyCookies(driver):
+    driver.find_element(By.CSS_SELECTOR, "#eucookielaw > div.conCookie > a:nth-child(1)").click()
+
+current_day = datetime.now().strftime('%A')
+
+day_action = {
+    'Monday': 'WOD', # friday class
+    'Tuesday': 'SUPERWOD', # saturday class
+    'Wednesday': 'WOD SUNDAY', # sunday class
+    'Thursday': 'WOD', # monday class
+    'Friday': 'WEIGHTLIFTING', # tuesday class
+    'Saturday': 'ENDURANCE', # wednesday class
+    'Sunday': 'WOD' # thursday class
+}
+
+classToBook = day_action.get(current_day, "WOD")
+
 
 try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
+    driver.get("https://aimharder.com/login")
 
+    denyCookies(driver)
 
-if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
+    email_field = driver.find_element(By.NAME, "mail")
+    password_field = driver.find_element(By.NAME, "pw")
 
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+    email_field.send_keys(email)
+    password_field.send_keys(pwd)
+
+    password_field.send_keys(Keys.RETURN)
+
+    time.sleep(5)
+
+    driver.find_element(By.CSS_SELECTOR, "#menuSwiperCon > div.swiper-wrapper > div > a.ahMenuOp.ahPicReservations").click()
+    button = driver.find_element(By.ID, "nextDay")
+
+    for i in range(daysRange):
+        button.click()
+        time.sleep(1)
+
+    driver.find_element(By.CSS_SELECTOR, "#select2-filtroClases-container").click()
+
+    input_field = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "/html/body/span/span/span[1]/input"))
+    )
+    input_field.send_keys(classToBook)
+    input_field.send_keys(Keys.ENTER)
+
+    # TODO improve the book button picker
+    # driver.find_element(By.CSS_SELECTOR, "#bloqueClass472329 > div.controlesClase > a:nth-child(2)").click()
+    reservar_link = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "Reservar"))
+    )
+    reservar_link.click()
+
+    print(classToBook + ' was booked succesfully!')
+
+    time.sleep(5)
+
+finally:
+    driver.quit()
